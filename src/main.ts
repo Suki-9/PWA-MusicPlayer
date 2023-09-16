a856ee28aa56b75657b8125233156e949f0415bb
suki-9 <suki.music2004@gmail.com>
2023-09-17 05:38:02 +0900

    setup template!

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// css ---------------------------------------///
+import "./Style/main.css";
 
-createApp(App).mount('#app')
+// ts module ---------------------------------///
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+
+createApp(App).use(router).mount("#app");
